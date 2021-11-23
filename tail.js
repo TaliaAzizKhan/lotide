c41a8f520f27e26ad65bcfcbd96d7d39db8409e3
Talia Aziz Khan <talyaaziz@gmail.com>
2021-11-23 08:46:51 +0000

    updated

--- tail.js
+++ tail.js
@@ -6,18 +6,26 @@ const assertEqual = function(actual, expected) {
   } else {
     console.log(`🛑🛑🛑  Assertion failed: ${actual} !== ${expected}`);
   }
-  
 };
-
 // TEST CODE
-
-
 // Returns the tail of an array. ie, everything after the first element.
-
 
 const tail = function(array) {
   return array.slice(1);
 };
 
 const result = tail(["Hello", "Lighthouse", "Labs"]);
+
+assertEqual(result.length, 2); // pass
+assertEqual(result[0], "Lighthouse"); // pass
+assertEqual(result[1], "Labs"); //pass
 assertEqual(result, ["Lighthouse", "Labs"]); // => will always fail!
+
+
+
+// Test Case: Check the original array 
+const words = ["Yo Yo", "Lighthouse", "Labs"];
+tail(words); // no need to capture the return value since we are not checking it
+assertEqual(words.length, 3); // pass (original array should still have 3 elements!)
+
+
