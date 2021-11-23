83ed96a0cebb0bb6df4813b87d9a4badc8ad170c
Talia Aziz Khan <talyaaziz@gmail.com>
2021-11-23 04:32:46 +0000

    head.js added

---
+++ head.js
@@ -0,0 +1,27 @@
+
+// FUNCTION IMPLEMENTATION
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log(`✅✅✅ Assertion passed: ${actual} === ${expected}`);
+  } else {
+    console.log(`🛑🛑🛑  Assertion failed: ${actual} !== ${expected}`);
+  }
+  
+};
+
+// TEST CODE
+assertEqual("Lighthouse Labs", "Bootcamp");
+assertEqual(1, 1);
+
+
+// Returns the head of an array.
+// Returns 'undefined' for empty array
+// jshint esversion: 6
+
+const head = function(array) {
+  return array[0];
+};
+
+
+assertEqual(head([5,6,7]), 5);
+assertEqual(head(["Hello", "Lighthouse", "Labs"]), "Hello");
