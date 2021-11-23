62e141d1aff9c2771af4253a985d335a6dfff254
Talia Aziz Khan <talyaaziz@gmail.com>
2021-11-23 04:01:34 +0000

    assertEqual added

---
+++ assertEqual.js
@@ -0,0 +1,16 @@
+
+// FUNCTION IMPLEMENTATION
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log(`✅✅✅ Assertion passed: ${actual} === ${expected}`);
+  } else {
+    console.log(`🛑🛑🛑  Assertion failed: ${actual} !== ${expected}`);
+  }
+  
+};
+
+// TEST CODE
+assertEqual("Lighthouse Labs", "Bootcamp");
+assertEqual(1, 1);
+
+
