bf5984dc229f76a2df7eb0c92ae94e50c4f2ac25
Talia Aziz Khan <talyaaziz@gmail.com>
2021-11-24 04:09:13 +0000

    eqArray added

---
+++ eqArrays.js
@@ -0,0 +1,34 @@
+// FUNCTION IMPLEMENTATION
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log(`✅✅✅ Assertion passed: ${actual} === ${expected}`);
+  } else {
+    console.log(`🛑🛑🛑  Assertion failed: ${actual} !== ${expected}`);
+  }
+  
+};
+
+
+//Code
+
+
+const eqArrays = function(arr1, arr2) {
+  let result = true;
+  if (arr1.length === arr2.length) {
+      
+    for (let i = 0; i < arr1.length; i++) {
+      if (arr1[i] !== arr2[i]) {
+        result = false;
+      }
+    }
+  } else {
+    result = false;
+  }
+  return result;
+};
+
+
+assertEqual(eqArrays([1, 2, 3], [1, 2, 3]), true); // => true
+assertEqual(eqArrays([1, 2, 3], [3, 2, 1]), true); // => false
+assertEqual(eqArrays(["1", "2", "3"], ["1", "2", "3"]), true); // => true
+assertEqual(eqArrays(["1", "2", "3"], ["1", "2", 3]), true); // => false
