bc6e814f0bdddf38fb028482096d6e680acfff0f
TaliaAzizKhan <92403710+TaliaAzizKhan@users.noreply.github.com>
2021-11-24 21:17:09 -0800

    Add files via upload

---
+++ letterPositions.js
@@ -0,0 +1,65 @@
+const assertArraysEqual = function(actual, expected) {
+  
+  if (eqArrays(actual, expected)) {
+    console.log(`✅✅✅ Assertion passed: ${actual} === ${expected}`);
+  } else {
+    console.log(`🛑🛑🛑  Assertion failed: ${actual} !== ${expected}`);
+  }
+};
+
+
+const eqArrays = function(arr1, arr2) {
+  let result = true;
+  if (arr1.length === arr2.length) {
+      
+    for (let i = 0; i < arr1.length; i++) {
+      if (arr1[i] !== arr2[i]) {
+        result = false;
+      }
+    }
+  } else {
+    result = false;
+  }
+  return result;
+};
+
+
+
+//Code:
+
+
+const letterPositions = function(sentence) {
+  let withoutSpace = sentence.replace(/\s/g, '');
+
+
+  let result = {};
+
+  for (let i = 0; i < withoutSpace.length; i++) {
+    let char = withoutSpace[i];
+    if (result[char]) {
+      result[char].push(i);
+    } else {
+      result[char] = [i];
+    }
+  }
+  return result;
+};
+
+
+const positions = letterPositions("Ta li a");
+console.log(positions);
+
+assertArraysEqual(positions, {T: [ 0 ], a: [ 1, 4 ], l: [ 2 ], i: [ 3 ]}); // => passed
+
+
+
+
+
+
+
+
+
+
+
+
+
