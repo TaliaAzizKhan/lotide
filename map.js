1aff2a2cfefd12fa1c7cd67a358e9066b402b89a
Talia Aziz Khan <talyaaziz@gmail.com>
2021-11-26 03:46:14 +0000

    updated

---
+++ map.js
@@ -0,0 +1,48 @@
+//assertArraysEqual
+
+const assertArraysEqual = function(actual, expected) {
+  
+  if (eqArrays(actual, expected)) {
+    console.log(`✅✅✅ Assertion passed: ${actual} === ${expected}`);
+  } else {
+    console.log(`🛑🛑🛑  Assertion failed: ${actual} !== ${expected}`);
+  }
+};
+
+//eqArrays
+
+const eqArrays = function(arr1, arr2) {
+  let result = true;
+  if (arr1.length === arr2.length) {
+      
+    for (let i = 0; i < arr1.length; i++) {
+      if (arr1[i] !== arr2[i]) {
+        result = false;
+      }
+    }
+  } else {
+    result = false;
+  }
+  return result;
+};
+
+
+
+// map
+
+const words = ["ground", "control", "to", "major", "tom"];
+
+
+const map = function(array, callback) {
+  
+  const results = [];
+  for (let item of array) {
+    results.push(callback(item));
+  }
+  return results;
+}
+
+
+const results1 = map(words, word => word[0]);
+console.log(results1);
+
